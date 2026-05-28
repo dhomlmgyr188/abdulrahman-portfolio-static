9363c1982bd62d1a242eb81f3c17fd0c0df47056
dhomlmgyr <dhomlmgyr@gmail.com>
2026-05-28 06:49:48 +0300

    pushh

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const swiper = new Swiper(".swiper-slider", {
-  // Optional parameters
   centeredSlides: true,
   slidesPerView: 1,
   grabCursor: true,
@@ -7,37 +6,57 @@ const swiper = new Swiper(".swiper-slider", {
   loop: true,
   mousewheel: false,
   keyboard: {
-    enabled: true
+    enabled: true,
   },
 
   // Enabled autoplay mode
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
 
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: false,
-    clickable: true
+    clickable: true,
   },
 
   // If we need navigation
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    prevEl: ".swiper-button-prev",
   },
 
   // Responsive breakpoints
   breakpoints: {
     640: {
       slidesPerView: 1.25,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     1024: {
       slidesPerView: 2,
-      spaceBetween: 20
-    }
-  }
+      spaceBetween: 20,
+    },
+  },
+});
+
+// collapse header
+const collapse = document.getElementById("collapse");
+const navbar = document.getElementById("navbar");
+const header = document.getElementById("header");
+const navButton = document.querySelectorAll(".js-hero-button");
+
+collapse.addEventListener("click", function () {
+  header.classList.add("active-is");
+  navbar.classList.add("is-active");
+  collapse.classList.toggle("hide");
+});
+
+navButton.forEach((link) => {
+  link.addEventListener("click", () => {
+    header.classList.remove("active-is");
+    navbar.classList.remove("is-active");
+    collapse.classList.remove("hide");
+  });
 });
