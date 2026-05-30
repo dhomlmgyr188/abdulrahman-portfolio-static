b9607683367d0f847492bad6b9f1505311fdc070
dhomlmgyr <dhomlmgyr@gmail.com>
2026-05-30 08:14:51 +0300

    حذف شيفرات اضافية ليس لها داعي

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-const swiper = new Swiper(".swiper-slider", {
-  centeredSlides: true,
-  slidesPerView: 1,
-  grabCursor: true,
-  freeMode: false,
-  loop: true,
-  mousewheel: false,
-  keyboard: {
-    enabled: true,
-  },
-
-  // Enabled autoplay mode
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: false,
-    clickable: true,
-  },
-
-  // If we need navigation
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    640: {
-      slidesPerView: 1.25,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-  },
-});
-
 // collapse header
 const collapse = document.getElementById("collapse");
 const navbar = document.getElementById("navbar");
@@ -61,11 +18,9 @@ navButton.forEach((link) => {
   });
 });
 
-// جلب جميع السلايدرز الموجودة في الصفحة
 const allSliders = document.querySelectorAll(".c-wrapper-slider");
 
 allSliders.forEach((wrapperSlider) => {
-  // البحث عن العناصر داخل السلايدر الحالي فقط باستخدام wrapperSlider بدلاً من document
   const carousel = wrapperSlider.querySelector(
     ".c-wrapper-slider__img-container__carousel",
   );
